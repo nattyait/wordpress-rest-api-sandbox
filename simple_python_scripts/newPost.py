209d290aab2ee5af4067b23de9c3c63fe3350251
Natty <natty@prontomarketing.com>
2013-08-20 19:34:20 +0700

    update more about how to use newPost API. Digged to the document and see what we can use now

--- simple_python_scripts/newPost.py
+++ simple_python_scripts/newPost.py
@@ -22,16 +22,24 @@ I will find more about attribute that I can use to create new post.
 
 ---- payload to use ----
 ID: for update
+title: Post title
 parent: Use for page
-date: don't know yet how to parse it here
-author: don't know yet how to parse it here
-menu_order: User for page
+date: don't know yet how to parse it here, to be continued
+author: don't know yet how to parse it here, to be continued
+menu_order: order number (use for page)
 password: any string,
 ping_status: open|closed
-slug: string
+slug: Post slug (URL identifier)
+content_raw: Post content
+status: "draft","pending","private","publish","trash" (draft is default if no value set)
+type: post type
+excpert_raw: excerpt
+name: post name
+comment_status: open|closed|registered_only
 
 ---- can't pass tru json yet ----
-sticky: don't know yet how to parse it here
+from code https://github.com/rmccue/WP-API/blob/master/lib/class-wp-json-posts.php
+sticky: boolean, to be continued
 
 ---- not yet implement ----
 terms
@@ -44,11 +52,11 @@ https://github.com/rmccue/WP-API/blob/master/docs/schema.json
 
 payload =  {"title":"Hello Request",
 			"content_raw":"Natty Content", 
-			"status":"publish", #"draft","pending","private","publish","trash" (draft is default if no value set)
+			"status":"publish",
 			"excerpt_raw": "This is excerpt",
 			"type": "post",
 			"name":"hello-hello",
-			"comment_status":"closed", # open|closed|registered_only
+			"comment_status":"closed", 
 			}
 headers = {'content-type': 'application/json'}
 
