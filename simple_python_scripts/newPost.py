f51202e0455ab1e76dd0d3f39851a80a6f878f61
Nattanicha <nattanicha@gmail.com>
2013-08-20 17:45:38 +0700

    comment about post object that I can use but doesn't work as same as documentation

--- simple_python_scripts/newPost.py
+++ simple_python_scripts/newPost.py
@@ -6,7 +6,20 @@ from requests.auth import HTTPBasicAuth
 rest_api = "/posts"
 
 url = "http://"+ domain + "/"+ api_path + rest_api
-#payload = {"post_title":"Hello World!","post_content":"Content"}
+
+'''
+From this doc https://github.com/rmccue/WP-API/blob/master/docs/routes.md
+It said the post object should be
+
+Content-Type: application/json
+{"post_title":"Hello World!","post_content":"Content"}
+
+But it doesn't work for me. I have to dig in the code and found that I have to use
+the payload as the line below.
+
+I will find more about attribute that I can use to create new post.
+'''
+
 payload =  {"title":"Hello Request","content_raw":"Natty Content", "status":"publish"}
 headers = {'content-type': 'application/json'}
 
